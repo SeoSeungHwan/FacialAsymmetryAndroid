e1bc1efa3618549458ad159c45b947d18f815c18
tmdgh <tmdghks3264@naver.com>
2021-06-27 01:23:09 +0900

    Take a picture and add to ImageView

--- app/src/main/java/com/example/facialasymmetryandroid/MainActivity.kt
+++ app/src/main/java/com/example/facialasymmetryandroid/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.example.facialasymmetryandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var permissionlistener: PermissionListener = object : PermissionListener {
+            override fun onPermissionGranted() {
+                camera_btn.setOnClickListener {
+                    Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                        takePictureIntent.resolveActivity(packageManager)?.also {
+                            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                        }
+                    }
+                }
+
+                gallery_btn.setOnClickListener {
+
+                }
+
+                submit_btn.setOnClickListener {
+
+                }
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        TedPermission.with(this)
+            .setPermissionListener(permissionlistener)
+            .setRationaleMessage("앱의 기능을 사용하기 위해서는 권한이 필요합니다.")
+            .setDeniedMessage("[설정] > [권한] 에서 권한을 허용할 수 있습니다.")
+            .setPermissions(
+                android.Manifest.permission.INTERNET,
+                android.Manifest.permission.CAMERA,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .check()
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            Glide.with(this)
+                .load(imageBitmap)
+                .into(imageView);
+        }
     }
 }
+
